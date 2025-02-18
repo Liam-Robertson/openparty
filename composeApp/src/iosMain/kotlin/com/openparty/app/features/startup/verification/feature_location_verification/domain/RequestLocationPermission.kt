8c42862c67196cbccd6fcc09159356bf3a13f4f6
Liam Robertson <liam@Liams-Mac-mini.local>
2025-02-18 08:04:25 +0000

    iphone compiles for location, doesn't wokr yet

--- composeApp/src/iosMain/kotlin/com/openparty/app/features/startup/verification/feature_location_verification/domain/RequestLocationPermission.kt
+++ composeApp/src/iosMain/kotlin/com/openparty/app/features/startup/verification/feature_location_verification/domain/RequestLocationPermission.kt
@@ -1,30 +1,31 @@
-// File: composeApp/src/iosMain/kotlin/com/openparty/app/features/startup/verification/feature_location_verification/RequestLocationPermission.ios.kt
+// File: composeApp/src/iosMain/kotlin/com/openparty/app/features/startup/verification/feature_location_verification/domain/RequestLocationPermission.ios.kt
+
 package com.openparty.app.features.startup.verification.feature_location_verification.domain
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import platform.CoreLocation.CLLocationManager
-import platform.CoreLocation.CLLocationManagerDelegateProtocol
-import platform.CoreLocation.kCLAuthorizationStatusAuthorizedAlways
-import platform.CoreLocation.kCLAuthorizationStatusAuthorizedWhenInUse
-import platform.CoreLocation.CLAuthorizationStatus
-//import platform.Foundation.NSObject
-
-//class LocationManagerDelegate(private val onResult: (Boolean) -> Unit) : NSObject(), CLLocationManagerDelegateProtocol {
-//    override fun locationManager(manager: CLLocationManager, didChangeAuthorizationStatus: CLAuthorizationStatus) {
-//        when (didChangeAuthorizationStatus) {
-//            kCLAuthorizationStatusAuthorizedAlways, kCLAuthorizationStatusAuthorizedWhenInUse -> onResult(true)
-//            else -> onResult(false)
-//        }
-//    }
-//}
+import platform.CoreLocation.*
+import platform.darwin.NSObject
 
 @Composable
 actual fun RequestLocationPermission(permission: String, onResult: (Boolean) -> Unit) {
     LaunchedEffect(Unit) {
-//        val locationManager = CLLocationManager()
-//        val delegate = LocationManagerDelegate(onResult)
-//        locationManager.delegate = delegate
-//        locationManager.requestWhenInUseAuthorization()
+        val locationManager = CLLocationManager()
+
+        val delegate = object : NSObject(), CLLocationManagerDelegateProtocol {
+            override fun locationManager(
+                manager: CLLocationManager,
+                didChangeAuthorizationStatus: CLAuthorizationStatus
+            ) {
+                when (didChangeAuthorizationStatus) {
+                    kCLAuthorizationStatusAuthorizedAlways,
+                    kCLAuthorizationStatusAuthorizedWhenInUse -> onResult(true)
+                    else -> onResult(false)
+                }
+            }
+        }
+
+        locationManager.delegate = delegate
+        locationManager.requestWhenInUseAuthorization()
     }
 }
